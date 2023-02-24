6e95f148a462d6597ee79974c0158d06f070a838
Amo-you <125539376+Amo-you@users.noreply.github.com>
2023-02-24 22:45:08 +0800

    排版

--- js/all.js
+++ js/all.js
@@ -2,23 +2,18 @@ new Vue({
     el: '#app',
     data: {
       focus:false,
-      message: 'Hover Me!'
     }, 
     methods: {
-      mouseover: function(refName){
-        console.log(refName)
+      mouseover: function(){
         this.focus = true
-        this.message = 'Good!'
       },    
       mouseleave: function(){
         this.focus = false
-        this.message = 'Hover Me!'
       }
     }
   })
   const navLinks = document.querySelectorAll('.nav-item')
   const menuToggle = document.getElementById('navBarBtn')
-    
   navLinks.forEach((event) => {
     event.addEventListener('click', () => { menuToggle.click() })
   })
