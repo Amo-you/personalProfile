15a7406c6cb6eb50da5aba62aea9e302971289c1
Amo-you <125539376+Amo-you@users.noreply.github.com>
2023-02-25 01:38:08 +0800

    JS判斷使用者裝置

--- js/all.js
+++ js/all.js
@@ -23,7 +23,13 @@ navA.forEach((element) => {
   element.addEventListener('click', ()=>{
     const goTo = element.dataset.to
     const toDiv = '#' + goTo
+    if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
       $("html,body").animate({scrollTop:$(toDiv).offset().top - 300},0)
+    } else {
+      $("html,body").animate({scrollTop:$(toDiv).offset().top},0)
+    }
+    //$("body,html")蘋果瀏覽器不會動
+    
   })
 })
 
